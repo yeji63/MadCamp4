2b9bef71d743382ea9c17e66de58809c56ea72cc
yeji <yejiyoun@sookmyung.ac.kr>
2021-07-27 15:55:09 +0900

    letter direct

--- public/index.js
+++ public/index.js
@@ -14,39 +14,49 @@ if(!firebase.apps.length){
 }
 const db = firebase.firestore();
 
-const total = 2;
+const total = 3;
 const rand = Math.floor(Math.random() * total); //0<=rand<total
 
 const username = localStorage.getItem('username');
 const gender = localStorage.getItem('gender');
 
+var temp;
+var check=0;
 
 db.collection("travel").where('num', '==', rand).get()
   .then(function(querySnapshot){
       querySnapshot.forEach(function(doc){
         console.log(doc.data());
           
-        var temp = `<p class="letter1" style="font-size: 35px;">Dear ${gender} ${username}</p>
+        temp = `<p class="letter1" style="font-size: 35px;">Dear ${gender} ${username}</p>
             <p class="letter2">Welcome.</p>
             <p class="letter2">We are pleased to inform you that today,</p>
             <p class="letter3">you will be joining our facinating trip to <b>"${doc.data().place}"</b></p> 
-            <p class="letter4">Your mission is to find the <b>"${doc.data().landmark}"</b>.</p>
+            <p class="letter4">Your mission is to find the <b>"${doc.data().landmark}"</b></p>
             <p class="letter4">Good luck on your trip!</p>  
             <p class="letter5">Yours sincerely,</p>
             <p class="letter6" style="font-family: 'Homemade Apple', cursive;">MadCamp4</p>`;
+       
 
-        $('.letter').append(temp)
-
-
-
-          localStorage.setItem('place', doc.data().place);
-          localStorage.setItem('landmark', doc.data().landmark);
-          localStorage.setItem('startCoordLat', doc.data().startCoord._lat);
-          localStorage.setItem('startCoordLng', doc.data().startCoord._long);
-          localStorage.setItem('destCoordLat', doc.data().destCoord._lat);
-          localStorage.setItem('destCoordLng', doc.data().destCoord._long);
+        localStorage.setItem('place', doc.data().place);
+        localStorage.setItem('landmark', doc.data().landmark);
+        localStorage.setItem('startCoordLat', doc.data().startCoord._lat);
+        localStorage.setItem('startCoordLng', doc.data().startCoord._long);
+        localStorage.setItem('destCoordLat', doc.data().destCoord._lat);
+        localStorage.setItem('destCoordLng', doc.data().destCoord._long);
       });
   })
   .catch(function(error){
       console.log("Error getting documents: ", error);
   });
+
+document.addEventListener('click', function(){
+    if (check==0){
+        $('.letter').append(temp)
+    }
+    else if (check==1){
+        location.href="streetview.html"
+    }
+    check++;
+})
+
