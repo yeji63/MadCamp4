64cdbc8cb94fdf88892c54d1bf894fdb27421a91
yeji <yejiyoun@sookmyung.ac.kr>
2021-07-27 23:24:11 +0900

    add letter paper & update alert

--- public/streetview.js
+++ public/streetview.js
@@ -37,7 +37,7 @@ function initPano() {
 
         if(Math.abs(newLat-destCoordLat)<0.001 && Math.abs(newLng-destCoordLng)<0.001) {
             if(!succ){
-                let ret = confirm(`Congratulations, you've found` + landmark + `! Would you like to return home?`);
+                let ret = confirm(`Congratulations, you've found ` + landmark + `! Would you like to return home?`);
                 if(ret) console.log(`return home`);
                 else console.log(`want to play more`);
                 console.log('current position: ', newLat, newLng);
