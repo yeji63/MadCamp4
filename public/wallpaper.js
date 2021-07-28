2d470dfffce341df52477691df4d315909e0c1a3
jjjunyeong <jjjunyeong@gmail.com>
2021-07-28 19:10:17 +0900

    add wallpaper, add hover

--- public/wallpaper.js
+++ public/wallpaper.js
@@ -1,9 +1,17 @@
-const travels = localStorage.getItem('travels');
+var travels = new Array();
+travels = localStorage.getItem('travels').split(",");
 console.log(travels);
-for(let i = 0; i < travels.length; i++){
-    if(travels[i]){
-        //컬러 사진 보여주기
+
+for(let i = 0; i < 6; i++){
+    console.log(travels[i]);
+    if(travels[i] == "true"){
+        var imagename = "img" + i;
+        console.log(imagename);
+        document.getElementById(imagename).style.display = "block";
     }else{
-        //흑백 사진 보여주기
+        var imagename = "img" + (i+10);
+        console.log(imagename);
+        document.getElementById(imagename).style.display = "block";
     }
 }
+
